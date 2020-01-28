d1a1a6976d14df1f015b4320cb704174ff7d68cc
Sergei Sobolev <sobolevsv@gmail.com>
2020-01-28 22:03:56 +0300

    test win

---
+++ goroutines/counter/counter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	var counter int64 = 0
+	for i := 0; i < 20; i++ {
+		go func(id int) {
+			for {
+				atomic.AddInt64(&counter, 1)
+				//fmt.Printf("%v -  %v\n", id, counter)
+			}
+		}(i)
+	}
+	for {
+		if atomic.LoadInt64(&counter) >= 10000 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Printf("counter: %v\n", counter)
+}
