97759c92c50812399d0df674618a78fa15b97568
Sergei Sobolev <sobolevsv@gmail.com>
2020-02-03 22:42:00 +0300

    test

--- goroutines/hello_world/hello.go
+++ goroutines/hello_world/hello.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-
-	go func() {
-		fmt.Println("Hello ")
-	}()
-	time.Sleep(1 * time.Second)
-	fmt.Println("world")
+	fmt.Println("Hello world")
 }
